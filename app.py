30449a9a30090c6cfb70fcb281d2a685cd707471
Carsten <39696700+CarstenProjects@users.noreply.github.com>
2022-12-02 12:50:57 +0100

    Project Setup

---
+++ app.py
@@ -0,0 +1,35 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///colors.db")
+
+# Make sure responses aren't cached
+@app.after_request
+def after_request(response):
+    """Disable caching"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Homepage
+@app.route("/")
+def index():
+    """Show colors"""
+    return render_template("index.html")
+
