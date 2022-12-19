fda99471525bb550a54e965d073a9d1af43c2445
Carsten Jung <39696700+CarstenJung@users.noreply.github.com>
2022-12-19 09:43:44 +0100

    ..

--- static/main.js
+++ static/main.js
@@ -1,28 +1,25 @@
-// input type file scan
-document.getElementById('image').onchange = function () {
+// Save image from scan page
+if (document.querySelector('.scan')) {
+  document.getElementById('image').onchange = function () {
     // Show loaded image
     let loadedImg = document.getElementById('loaded_img')
 
     loadedImg.style.opacity = 1;
     loadedImg.src = URL.createObjectURL(this.files[0])
-  };
-
-
-
-/* $(document).on('submit','#form_test',function(e) {
-  console.log('hello');
-  e.preventDefault();
-  $.ajax({
-    type:'POST',
-    url:'/console',
-    data:{
-      todo:$("#todo").val()
-  },
-  success:function() {
-    alert('saved');
   }
-  })
-}); */
+}
 
+// Copy Color RGB from mycolors
+if (document.querySelector('.mycolors')) {
+  var savedColor = document.querySelectorAll('.saveColor_btn');
 
+  savedColor.forEach((btn) => {
+    btn.addEventListener('click', saveColor)
+  });
 
+  function saveColor() {
+    const color_rgb = document.querySelector('.mycolors_rgb');
+    navigator.clipboard.writeText(color_rgb.innerText);
+    console.log(color_rgb.innerHTML)
+  }
+}
